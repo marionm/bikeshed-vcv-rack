8f72dc415380dfad9d5d2772c5edc39e2c6728ef
Mike Marion <mike.marion@gmail.com>
2026-07-03 14:31:38 -0500

    Add units to scale tooltip

--- src/modules/EntropyBase/ScaleParamQuantity.cpp
+++ src/modules/EntropyBase/ScaleParamQuantity.cpp
@@ -6,10 +6,10 @@ std::string ScaleParamQuantity::getString() {
   float value = getValue();
 
   if (value == 0) {
-    return getLabel() + ": 0";
+    return getLabel() + ": 0 V";
   } else if (value > 0) {
-    return string::f("%s: 0 to %.2f", getLabel(), value * 10.f);
+    return string::f("%s: 0 V to %.2f V", getLabel(), value * 10.f);
   } else {
-    return string::f("%s: %.2f to %.2f", getLabel(), value * 5.f, value * -5.f);
+    return string::f("%s: %.2f V to %.2f V", getLabel(), value * 5.f, value * -5.f);
   }
 }
