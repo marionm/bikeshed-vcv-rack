4a40771bab3fe5f6a1fc460d69fb55880545abe1
Mike Marion <mike.marion@gmail.com>
2026-07-08 01:38:57 -0500

    Commit 1 to fix filename casing

--- src/bikeshed.cpp
+++
@@ -1,9 +0,0 @@
-#include "Bikeshed.hpp"
-
-Plugin* pluginInstance;
-
-void init(Plugin* p) {
-	pluginInstance = p;
-	p->addModel(entropyPoolModel);
-	p->addModel(entropyPuddleModel);
-}
--- src/bikeshed.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <rack.hpp>
-
-using namespace rack;
-
-extern Model* entropyPoolModel;
-extern Model* entropyPuddleModel;
-extern Plugin* pluginInstance;
